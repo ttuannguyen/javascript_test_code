92b5b2a3e1107232f0a433f8dc928cf858f81185
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-10-04 00:10:15 -0700

    add a test to yield a correct sum if an array of numeric string values is provided

--- basic-testing-frontend/src/math.js
+++ basic-testing-frontend/src/math.js
@@ -2,7 +2,9 @@ export function add(numbers) {
   let sum = 0;
 
   for (const number of numbers) {
-    sum += number;
+    // sum += number;
+    // fixed our code to pass all tests
+    sum += +number;
   }
   return sum;
 }
--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -14,3 +14,28 @@ test('sums up all number values in an array', () => {
     // define our expectations
     expect(result).toBe(expectedResult);
 });
+
+test("yields NaN if at least one invalid number is provided", () => {
+    // Arrange
+    const inputs = ['invalid', 1];
+
+    // Act
+    const result = add(inputs);
+
+    // Assert
+    expect(result).toBeNaN();
+});
+
+test("yeilds a correct sum if an array of numeric string values is provided", () => {
+    // Arrange
+    const numbers = ['1', '2'];
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    const expectedResult = numbers.reduce((preValue, curValue) => +preValue + +curValue, 0); // Adding + to force a converstion to a real number
+
+    expect(result).toBe(expectedResult)
+})
+
