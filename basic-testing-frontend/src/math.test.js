6a6da7e2490bdd40c5f9fe417a9d37ec16c8f3f1
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-10-03 19:14:46 -0700

    refactor the test for add function to make the calculation for expected result more programmatic

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -3,8 +3,14 @@ import { add } from './math';
 
 
 test('sums up all number values in an array', () => {
+    // Arrange
     const numbers = [1, 2, 3];
+
+    // Act
     const result = add(numbers);
+
+    // Assert
+    const expectedResult = numbers.reduce((preValue, curValue) => preValue + curValue, 0);
     // define our expectations
-    expect(result).toBe(6);
+    expect(result).toBe(expectedResult);
 });
